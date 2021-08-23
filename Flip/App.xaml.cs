f04d13b57c4c31eec0645698b40b1b5656ac09b3
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-08-23 15:27:44 +0900

    초기 커밋.

---
+++ Flip/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using Flip.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Flip
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+
+        }
+    }
+}
---
+++ Flip/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Prism.Mvvm;
+
+namespace Flip.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            Title = "Flip";
+        }
+    }
+}
